26afc4ccf3dc7479e8c5db1b502845a8a16ce65d
vburmus <burmusvolod@gmail.com>
2024-02-12 12:06:40 +0100

    feat(entities) added brand, city and region

---
+++ src/main/java/com/vburmus/batch/car/brand/Brand.java
@@ -0,0 +1,46 @@
+package com.vburmus.batch.car.brand;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Brand {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Brand brand = (Brand) o;
+        return new EqualsBuilder()
+                .append(id, brand.id)
+                .append(name, brand.name)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(id)
+                .append(name)
+                .toHashCode();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Brand {id=%d, name=%s}", id, name);
+    }
+}
---
+++ src/main/java/com/vburmus/batch/region/Region.java
@@ -0,0 +1,46 @@
+package com.vburmus.batch.region;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Region {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Region region = (Region) o;
+        return new EqualsBuilder()
+                .append(id, region.id)
+                .append(name, region.name)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(id)
+                .append(name)
+                .toHashCode();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Region {id=%d, name=%s}", id, name);
+    }
+}
