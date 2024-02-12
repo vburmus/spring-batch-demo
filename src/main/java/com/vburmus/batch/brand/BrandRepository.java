722e81a016a910343c57c3fef698273c03b9b888
vburmus <burmusvolod@gmail.com>
2024-02-12 16:13:14 +0100

    feat(repositories) added jpa repositories

---
+++ src/main/java/com/vburmus/batch/brand/BrandRepository.java
@@ -0,0 +1,8 @@
+package com.vburmus.batch.brand;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BrandRepository extends JpaRepository<Brand, Integer> {
+}
