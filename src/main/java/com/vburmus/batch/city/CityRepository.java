067ed9cb25ebb85d920a27955ecf3954309c0112
vburmus <burmusvolod@gmail.com>
2024-02-13 16:12:23 +0100

    feat(repositories) added methods to search by name

--- src/main/java/com/vburmus/batch/city/CityRepository.java
+++ src/main/java/com/vburmus/batch/city/CityRepository.java
@@ -3,6 +3,9 @@ package com.vburmus.batch.city;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CityRepository extends JpaRepository<City, Integer> {
+    Optional<City> findByName(String name);
 }
--- src/main/java/com/vburmus/batch/region/RegionRepository.java
+++ src/main/java/com/vburmus/batch/region/RegionRepository.java
@@ -3,6 +3,9 @@ package com.vburmus.batch.region;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RegionRepository extends JpaRepository<Region, Integer> {
+    Optional<Region> findByName(String name);
 }
